149e74ad27a584383562a853ed5e3ece349fa45b
Vincent <vincent14@student.umn.ac.id>
2023-06-22 15:13:47 +0700

    creating home & setup components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Promptopia is a open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
 
-export default Home
+      {/* feed */}
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/Profider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profider = () => {
+  return (
+    <div>Profider</div>
+  )
+}
+
+export default Profider
