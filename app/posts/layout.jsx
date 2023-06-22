de9cc96e0c95677ffa738492be580a2d89c33686
Vincent <vincent14@student.umn.ac.id>
2023-06-22 15:05:35 +0700

    project promptopia setup

--- app/posts/layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const layout = () => {
-  return (
-    <div>Navigate To Top</div>
-  )
-}
-
-export default layout
--- app/posts/new/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>New Post</div>
-  )
-}
-
-export default page
